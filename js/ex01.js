65a203c02702827763d526c69ef5c7576fac57df
brunofbataglin <brunofbwork1111@gmail.com>
2025-09-22 17:23:52 -0300

    novo
    tentativas e contador

--- js/ex01.js
+++ js/ex01.js
@@ -1,14 +1,15 @@
 
 
 var x = parseInt( Math.random() *100)
-
+var tentativas = 0
 do{
 
-var valor= parseInt(prompt("Digite um número entre 1 e 100"))
+var valor= parseInt(prompt("Digite um número entre 1 e 100. Você tem 10 tentativas. Até agora foram feitas " + tentativas + " tentativas."))
 
 
 if(x == valor){
     alert("Parabéns, Você Acertou!")
+
 }
 
 else if(valor > x && valor <= 100 && valor>=1){
@@ -22,9 +23,9 @@ else if(valor < x && valor <= 100 && valor>=1){
 else{
     alert("Número inválido!")
 }
+tentativas++
 
-
-} while(x != valor)
+} while(x != valor && tentativas < 10)
 
 
 
